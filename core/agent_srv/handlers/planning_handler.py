296b1482638c668093112fcdccf69b48388cae5b
ZhangShuui <1262426565@qq.com>
2025-02-08 17:05:28 +0800

    add anticipation skeleton

--- core/agent_srv/handlers/planning_handler.py
+++ core/agent_srv/handlers/planning_handler.py
@@ -10,6 +10,14 @@ import traceback
 
 class PlanningHandler(BaseHandler):
     async def generate_daily_objective(self, state: RunningState):
+        await asyncio.gather(
+            self.generate_general_daily_objective(state),
+            self.generate_trading_objective(state),
+        )
+
+        await self.merge_objectives(state)
+
+    async def generate_general_daily_objective(self, state: RunningState):
         obj_planner = self.create_planner(
             obj_planner_prompt,
             state.get("character_stats", {}).get("model_type"),
@@ -257,3 +265,92 @@ class PlanningHandler(BaseHandler):
         )
         if state.get("instance"):
             state["instance"].log_message("received", response)
+
+    async def generate_trading_objective(self, state: RunningState):
+        obj_planner = self.create_planner(
+            trade_planner_prompt,
+            state.get("character_stats", {}).get("model_type"),
+            DailyObjective,
+            0.7,
+        )
+        dev_dict = agent_api.request_sync(
+            method="GET", endpoint=f"/production_path/{state['userid']}"
+        )
+        state["meta"]["production_graph"] = format_level_graph(
+            dev_dict,
+            state["character_stats"]["inventory"],
+            state["character_stats"]["energy"],
+        )
+
+        last_decision = agent_api.request_sync(
+            method="GET",
+            endpoint="/action_log/",
+            params={"characterId": state["userid"], "count": 5},
+        )
+        daily_objectives_list = list(state["decision"]["daily_objective"])
+        if len(daily_objectives_list) > 0:
+            daily_objectives_list = daily_objectives_list[-1]
+        payload = {
+            "character_stats": format_character_data(
+                state["character_stats"],
+                fields=["money", "inventory"],
+            ),
+            "past_objectives": format_daily_obj(daily_objectives_list),
+            "past_reflection": (
+                last_decision.get("reflection", []) if last_decision else []
+            ),
+            "production_graph": state["meta"]["production_graph"],
+        }
+        planner_response = self.api_retry(
+            obj_planner.ainvoke,
+            payload,
+            state,
+            DailyObjective,
+        )
+        full_prompt = trade_planner_prompt.format(**payload)
+        logger.info("======generate_daily_objective======\n" + full_prompt)
+        state["decision"]["trade_objective"].append(planner_response.objectives)
+
+        logger.info(f"🌞 OBJ_PLANNER INVOKED with {planner_response.progress}")
+        logger.info(f"🌞 OBJ_PLANNER INVOKED with {planner_response.objectives}")
+
+        return {"current_pointer": "objectives_planner"}
+
+    async def merge_objectives(self, state: RunningState):
+        # merge objectives
+        merger = self.create_planner(
+            merger_prompt,
+            state.get("character_stats", {}).get("model_type"),
+            DailyObjective,
+            0.7,
+        )
+        daily_objectives_list = list(state["decision"]["daily_objective"])
+        last_daily_objective = daily_objectives_list[
+            -1
+        ]  # MUST HAVE AT LEAST 1 OBJECTIVE
+        last_trade_objective = state["decision"]["trade_objective"]
+        payload = {
+            "current_daily_objectives": format_daily_obj(last_daily_objective),
+            "past_daily_objectives": format_daily_obj(
+                daily_objectives_list[-2] if len(daily_objectives_list) > 1 else []
+            ),
+            "current_trading_objectives": format_daily_obj(last_trade_objective),
+            "additional_info": state.get("decision", {}).get("additional_info", ""),
+        }
+
+        print(merger_prompt.format(**payload))
+        merger_response = await self.api_retry(
+            merger.ainvoke,
+            payload,
+            state,
+            DailyObjective,
+        )
+
+        full_prompt = merger_prompt.format(**payload)
+
+        logger.info("======merge_objectives======\n" + full_prompt)
+        # state["decision"]["daily_objective"] = merger_response.objectives
+        logger.info(
+            f"🌞 MERGER INVOKED with {merger_response.progress}\n"
+            f" and\n {merger_response.objectives}"
+        )
