f87e4420d48426124fd9817f99a14023fa84821b
ZhangShuui <1262426565@qq.com>
2025-02-20 15:51:10 +0800

    refine action simulator

--- core/agent_srv/action_simulator.py
+++ core/agent_srv/action_simulator.py
@@ -31,7 +31,7 @@ class ActionSimulator:
         """
         Simulate the action list on the initial state, return the final state and reward
         """
-        self.action_runner.market_data = market_data
+        self.action_runner.init(market_data)
         state = copy.deepcopy(initial_state)
         state["inventory"] = {
             item.lower(): num for item, num in state["inventory"].items()
@@ -87,6 +87,21 @@ class ActionRunner:
             self.craft_location,
             self.trade_location,
         ) = self.load_craft_recipes()
+
+    def init(self, market_data):
+        self.market_data = market_data
+        self.prob_food = self.compute_food_prob(
+            [
+                "apple",
+                "pear",
+                "bread",
+                "apple_pie",
+                "fruit_salad",
+                "chicken_salad",
+                "beef_rice",
+                "sushi",
+            ]
+        )
 
     def load_craft_recipes(self):
         """
@@ -175,7 +190,7 @@ class ActionRunner:
             "mine",
             "orchard",
             "foodfactory",
-            "factory",
+            "semiconductorfactory",
             "garden",
             "policestation",
             "library",
@@ -183,6 +198,7 @@ class ActionRunner:
             "canteen",
             "ranch",
             "forest",
+            "minefactory",
         ]:
             # Current: Give up goto
             self.actions = []
@@ -675,11 +691,29 @@ class ActionRunner:
             "beef_rice": 50,
             "sushi": 30,
         }
-        choices = ["craft", "buy"]
-        choose_to = random.choice(choices)
-        food = random.choice(foods)
+        # Decide by probability and money:
+        # if the character has more money, they are more likely to choose to buy
+        # the more expensive the food, the less likely to choose to buy
 
+        # choose_to = random.choice(choices)
+        # food = random.choice(foods)
+        prob_buy = state["money"] / (state["money"] + 50)
+        choose_to = "buy" if random.random() < prob_buy else "craft"
+        food = random.choices(foods, weights=list(self.prob_food.values()))[0]
         num = recover_hungry // hungry_dict[food] + 1
+        if state["inventory"].get(food, 0) > 0:
+            if state["inventory"][food] >= num:
+                actions = [f"use {food} {num}"]
+                state["hungry"] = min(100, state["hungry"] + hungry_dict[food])
+                return actions
+            else:
+                actions = [f"use {food} {state['inventory'][food]}"]
+                state["hungry"] = min(
+                    100, state["hungry"] + state["inventory"][food] * hungry_dict[food]
+                )
+                num -= state["inventory"][food]
+                return actions
+
         if choose_to == "craft":
             actions = self.generate_craft_sequence_and_check(
                 state,
@@ -727,6 +761,23 @@ class ActionRunner:
             "Store Clerk": 25,
         }
         return salary_dict[occupation]
+
+    def compute_food_prob(self, foods):
+        """
+        Compute the probability of choosing to eat food
+        """
+        price_dict = {}
+        total_price = 0
+        for food in foods:
+            for item in self.market_data:
+                if item["itemName"] == food:
+                    price_dict[food] = item["price"]
+                    total_price += 1 / item["price"]
+
+        prob_dict = {}
+        for food in foods:
+            prob_dict[food] = 1 / price_dict[food] / total_price
+        return prob_dict
 
 
 if __name__ == "__main__":
